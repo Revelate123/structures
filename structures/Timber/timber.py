081999b51c5c9f6d7ad28c51b25819215ef3e18c
Thomas Duffett <thomasduffett4@gmail.com>
2025-08-22 20:12:58 +1000

    Fixed bugs in timber bending

--- structures/Timber/timber.py
+++ structures/Timber/timber.py
@@ -1,21 +1,28 @@
 import math
 
-import dataclasses
+from dataclasses import dataclass
 
-@dataclasses
+@dataclass
 class TimberBeam:
     length: float|None = None
     depth: float|None = None
     breadth: float|None = None
     φ_shear: float = 0.1
-    φ_bending: float = 0.1
+    φ_bending: float|None = None
     φ_compression: float = 0.1
     fb: float| None = None
-    
 
     def __post_init__(self):
-        self.Zxx = self.breadth*self.depth**2/6
-        self.Zyy = self.depth*self.breadth**2/6
+        if self.length is None:
+            raise ValueError("length is not set. This is the length of beam being considered between supports in mm.")
+        if self.depth is None:
+            raise ValueError("depth is not set. This is the depth of the beam in mm.")
+        if self.breadth is None:
+            raise ValueError("breadth is not set. This is the breadth of the beam in mm.")
+        if self.fb is None:
+            raise ValueError("fb is not set. This is in MPa")
+        if self.φ_bending is None:
+            raise("φ_bending not set.")
 
     def _bending(self,
                   loads=[],
@@ -49,11 +56,11 @@ class TimberBeam:
 
         k4 = self._calc_k4(seasoned=seasoned,moisture_content=moisture_content,verbose=verbose)
         k6 = self._calc_k6(latitude=latitude,verbose=verbose)
-        k9 = self._calc_k9(ncom=ncom, nmem=nmem,spacing=spacing,span=span)
+        k9 = self._calc_k9(ncom=ncom, nmem=nmem,spacing=spacing,span=span,verbose=verbose)
         k12 = self._calc_k12(pb=pb,restraint_location=restraint_location,Lay=Lay,out_of_plane=out_of_plane,verbose=verbose) 
-        Z = self._calc_Z(out_of_plane=out_of_plane)
+        Z = self._calc_Z(out_of_plane=out_of_plane,verbose=verbose)
 
-        Md = self.φ_bending * k4 * k6 * k9 * k12 * self.fb * Z
+        Md = self.φ_bending * k4 * k6 * k9 * k12 * self.fb * Z * 1e-6
         if verbose == True:
             print(f"Md: {Md} KNm")
         return Md
@@ -101,12 +108,12 @@ class TimberBeam:
 
     def _calc_k4(self,seasoned, moisture_content, verbose):
         """Computes k4 using AS1720.1-2010 Cl 2.4.2.2 & Cl 2.4.2.3"""
-        if not seasoned:
+        if  seasoned is None:
             raise ValueError("seasoned not set, set to True if using seasoned timber, and False otherwise")
         elif verbose:
             print(f"seasoned: {seasoned}")
         
-        if not moisture_content:
+        if moisture_content is None:
             raise ValueError("moisture_content not set, set to 15 if inside and 25 if outside. " \
             "Note: further investigation needed regarding moisture content values")
         elif verbose:
@@ -133,7 +140,7 @@ class TimberBeam:
     
     def _calc_k6(self, latitude, verbose):
         """Computes k6 using AS1720.1-2010 Cl 2.4.3"""
-        if not latitude:
+        if latitude is None:
             raise ValueError("latitude not set, set to True if located in coastal Queensland " \
             "north of latitude 25 degrees south or 16 degrees south elsewhere, and False otherwise.")
         elif verbose:
@@ -149,25 +156,30 @@ class TimberBeam:
     def _calc_k9(self,ncom,nmem,spacing,span,verbose):
         """Computes k9 using AS1720.1-2010 Cl 2.4.5.3"""
 
-        if not ncom:
+        if ncom is None:
             raise ValueError("ncom not set, this is the number of elements that are effectively fastened together to form a single group")
         elif verbose:
             print(f"ncom: {ncom}, number of members per group")
         
-        if not nmem:
+        if nmem is None:
             raise ValueError("nmem not set, this is the number of members that are discretely spaced parallel to each other")
         elif verbose:
             print(f"nmem: {nmem}, number of groups of members")
         
-        if nmem > 1 and not spacing:
+        if nmem > 1 and  spacing is None:
             raise ValueError("nmem greater than 1 but spacing between groups not set. This should be in mm.")
-        elif verbose:
+        elif verbose and nmem > 1 and  not spacing is None:
             print(f"spacing: {spacing} mm")
         
-        if nmem > 1 and not span:
+        if nmem > 1 and span is None:
             raise ValueError("nmem greater than 1 but span of members not set. This should be in mm.")
-        elif verbose:
+        elif verbose and nmem > 1 and not span is None:
             print(f"span: {span} mm")
+        
+        if nmem == 1 and ncom == 1:
+            if verbose:
+                print(f"k9: 1")
+            return 1
 
         table_2_7 = [0,1,1.14,1.2,1.24,1.26,1.28,1.3,1.31,1.32,1.33]
 
@@ -185,7 +197,7 @@ class TimberBeam:
     
     def _calc_k12(self, pb:float|None = None, restraint_location:int|None=None, Lay:float|None = None, fly_brace_spacing:int|None = None,out_of_plane:bool|None = None, verbose:bool = True):
         """Computes k12 using AS1720.1-2010 Cl """
-        if not pb:
+        if  pb is None:
             raise ValueError("pb not defined")
         elif verbose:
             print(f"pb: {pb}")
@@ -215,31 +227,45 @@ class TimberBeam:
             raise ValueError("restraint_location set to 3 but fly-brace spacing has not been set. This should be the number of members in the group"
             "[L,R), for example, if there are fly braces to every purlin," \
             " then fly_brace_spacing should be set to 1, if they alternate every 2nd purlin, it should be set to 2, etc.")
-        elif verbose:
+        elif verbose and restraint_location == 3 and not fly_brace_spacing is None:
             if fly_brace_spacing == 1:
                 print("fly braces connected to every restraint")
             elif fly_brace_spacing > 1:
                 print(f"fly bracing to every {fly_brace_spacing} restraints")
             
         cont_restrained = self._cont_restraint(pb=pb,Lay=Lay,verbose=verbose)
-        return self._calc_S1(pb=pb,restraint_location=restraint_location,Lay=Lay,fly_brace_spacing=fly_brace_spacing,cont_restrained=cont_restrained,verbose=verbose)
+        S1 =  self._calc_S1(pb=pb,restraint_location=restraint_location,Lay=Lay,fly_brace_spacing=fly_brace_spacing,cont_restrained=cont_restrained,verbose=verbose)
         
+        if pb * S1 <= 10:
+            k12 = 1
+        elif pb * S1 <= 20:
+            k12 = 1.5 - 0.05 * pb * S1
+        else:
+            k12 = 200/(pb*S1)**2
+        if verbose:
+            print(f"k12: {k12}")
+        return k12
 
     def _calc_S1(self,pb:float|None = None, restraint_location:int|None=None, Lay:float|None = None, fly_brace_spacing:int|None = None,cont_restrained:bool = False, verbose:bool = True):
         """ Calculates the beam slenderness S1 """
         if cont_restrained == True:
             if restraint_location == 1:
-                return 0
+                S1 = 0
             if restraint_location == 2:
-                return 2.25*self.depth/self.breadth
+                S1 = 2.25*self.depth/self.breadth
             if restraint_location == 3:
-                return (1.5 * self.depth/self.breadth) / ((math.pi*self.depth/fly_brace_spacing*Lay)**2+0.4)**0.5
-        else:
+                S1 = (1.5 * self.depth/self.breadth) / ((math.pi*self.depth/fly_brace_spacing*Lay)**2+0.4)**0.5
+        elif cont_restrained == False:
             if restraint_location == 1:
-                return 1.25*self.depth/self.breadth*(Lay/self.d)**0.5
+                S1 = 1.25*self.depth/self.breadth*(Lay/self.depth)**0.5
             if restraint_location == 2 or restraint_location == 3:
-                return (self.depth/self.breadth)**1.35 * (Lay/self.depth)**0.25
-        raise ValueError("_calc_S1 did not find a matching restraint case")
+                S1 = (self.depth/self.breadth)**1.35 * (Lay/self.depth)**0.25
+        else:
+            raise ValueError("_calc_S1 did not find a matching restraint case")
+
+        if verbose:
+            print(f"S1: {S1}")
+        return S1
         
 
     def _cont_restraint(self,pb:float|None = None,  Lay:float|None = None, verbose:bool = True):
@@ -250,12 +276,16 @@ class TimberBeam:
         return cont_restrained
 
         
-    def _calc_Z(self, out_of_plane):
+    def _calc_Z(self, out_of_plane,verbose):
         if out_of_plane is None:
             raise ValueError("out_of_plane not set.")
         if out_of_plane == True:
-            return self.depth*self.breadth**2/6
+            Z = self.depth*self.breadth**2/6
         elif out_of_plane == False:
-            return self.breadth*self.depth**2/6
+            Z = self.breadth*self.depth**2/6
         else:
             raise ValueError("error in _calc_Z")
+        
+        if verbose:
+            print(f"Z: {Z} mm3")
+        return Z
