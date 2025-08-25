cb5b5358313ec298156390098df43111c0934c8d
Thomas Duffett <thomasduffett4@gmail.com>
2025-08-25 17:26:24 +1000

    Added Timber column class

--- structures/Timber/timber.py
+++ structures/Timber/timber.py
@@ -23,48 +23,6 @@ class TimberBeam:
             raise ValueError("fb is not set. This is in MPa")
         if self.φ_bending is None:
             raise("φ_bending not set.")
-
-    def _bending(self,
-                  loads=[],
-                  seasoned=None,
-                    moisture_content=None,
-                    latitude = None,
-                      ncom=None,
-                        nmem = None,
-                          spacing = None,
-                            span = None,
-                              out_of_plane:bool|None = None,
-                              pb:float|None =None,
-                              restraint_location:int|None = None,
-                              Lay:float|None = None,
-                                Z:float|None = None,
-                                verbose=True):
-        """
-        Computes the bending capacity of a timber element using the methods
-        described in AS 1720 Cl 3.2
-
-        Args:
-            loads: List of applied loads in kN.
-            seasoned: True if seasoned timber is used and false otherwise.
-            moisture_content: precentage moisture content, given as whole numbers, e.g. for 15% set as 15.
-            latitude: True if located in coastal Queensland north of latitude 25 degrees south or 16 degrees south elsewhere, and False otherwise.
-            verbose: If True, print internal calculation details.
-
-        Returns:
-            A dictionary with bending capacities for different durations related to the factor k1
-        """
-
-        k4 = self._calc_k4(seasoned=seasoned,moisture_content=moisture_content,verbose=verbose)
-        k6 = self._calc_k6(latitude=latitude,verbose=verbose)
-        k9 = self._calc_k9(ncom=ncom, nmem=nmem,spacing=spacing,span=span,verbose=verbose)
-        k12 = self._calc_k12(pb=pb,restraint_location=restraint_location,Lay=Lay,out_of_plane=out_of_plane,verbose=verbose) 
-        Z = self._calc_Z(out_of_plane=out_of_plane,verbose=verbose)
-
-        Md = self.φ_bending * k4 * k6 * k9 * k12 * self.fb * Z * 1e-6
-        if verbose == True:
-            print(f"Md: {Md} KNm")
-        return Md
-
     def in_plane_bending(self,
                   loads=[],
                   seasoned=None,
@@ -105,6 +63,76 @@ class TimberBeam:
                              pb=pb, restraint_location=restraint_location,Lay=Lay,Z=Z,
                              out_of_plane=True if self.depth > self.breadth else False,
                              verbose=verbose)
+    
+    def shear(self, loads=[],seasoned=None, moisture_content=None, latitude=None, verbose=None):
+        """
+        Computes the shear capacity of a timber element using the methods
+        described in AS 1720 Cl 3.2.5
+
+        Args:
+            loads: List of applied loads in kN.
+            seasoned: True if seasoned timber is used and false otherwise.
+            moisture_content: precentage moisture content, given as whole numbers, e.g. for 15% set as 15.
+            latitude: True if located in coastal Queensland north of latitude 25 degrees south or 16 degrees south elsewhere, and False otherwise.
+            verbose: If True, print internal calculation details.
+
+        Returns:
+            A dictionary with bending capacities for different durations related to the factor k1
+        """
+        k4 = self._calc_k4(seasoned=seasoned,moisture_content=moisture_content,verbose=verbose)
+        k6 = self._calc_k6(latitude=latitude,verbose=verbose)
+        
+        As = 2/3*(self.breadth * self.depth)
+        if verbose:
+            print(f"As: {As} mm2")
+
+        Vd = self.φ_shear*k4*k6*self.fs*As
+        if verbose == True:
+            print(f"Vd = {Vd} KN (Not including k1)")
+        return Vd
+
+    def _bending(self,
+                  loads=[],
+                  seasoned=None,
+                    moisture_content=None,
+                    latitude = None,
+                      ncom=None,
+                        nmem = None,
+                          spacing = None,
+                            span = None,
+                              out_of_plane:bool|None = None,
+                              pb:float|None =None,
+                              restraint_location:int|None = None,
+                              Lay:float|None = None,
+                                Z:float|None = None,
+                                verbose=True):
+        """
+        Computes the bending capacity of a timber element using the methods
+        described in AS 1720 Cl 3.2
+
+        Args:
+            loads: List of applied loads in kN.
+            seasoned: True if seasoned timber is used and false otherwise.
+            moisture_content: precentage moisture content, given as whole numbers, e.g. for 15% set as 15.
+            latitude: True if located in coastal Queensland north of latitude 25 degrees south or 16 degrees south elsewhere, and False otherwise.
+            verbose: If True, print internal calculation details.
+
+        Returns:
+            A dictionary with bending capacities for different durations related to the factor k1
+        """
+
+        k4 = self._calc_k4(seasoned=seasoned,moisture_content=moisture_content,verbose=verbose)
+        k6 = self._calc_k6(latitude=latitude,verbose=verbose)
+        k9 = self._calc_k9(ncom=ncom, nmem=nmem,spacing=spacing,span=span,verbose=verbose)
+        k12 = self._calc_k12(pb=pb,restraint_location=restraint_location,Lay=Lay,out_of_plane=out_of_plane,verbose=verbose) 
+        Z = self._calc_Z(out_of_plane=out_of_plane,verbose=verbose)
+
+        Md = self.φ_bending * k4 * k6 * k9 * k12 * self.fb * Z * 1e-6
+        if verbose == True:
+            print(f"Md: {Md} KNm (Not inlcuding k1)")
+        return Md
+
+    
 
     def _calc_k4(self,seasoned, moisture_content, verbose):
         """Computes k4 using AS1720.1-2010 Cl 2.4.2.2 & Cl 2.4.2.3"""
@@ -266,6 +294,9 @@ class TimberBeam:
         if verbose:
             print(f"S1: {S1}")
         return S1
+    
+    def _calc_S2(self):
+        pass#TODO
         
 
     def _cont_restraint(self,pb:float|None = None,  Lay:float|None = None, verbose:bool = True):
@@ -289,3 +320,95 @@ class TimberBeam:
         if verbose:
             print(f"Z: {Z} mm3")
         return Z
+    
+class TimberColumn(TimberBeam):
+
+    def out_of_plane_compression(self):
+        pass
+
+    def in_plane_compression(self):
+        pass
+
+    def compression(self):
+        pass
+
+    def _compression(self,seasoned=None, moisture_content=None, latitude=None, fc=None, slenderness:function|None = None, g13=None, pc=None, La=None, out_of_plane = None, verbose=None):
+        """ 
+        Computes the compressive strength of a Timber column parallel to grain in accordance with AS1720.1 Cl 3.3.1
+
+        Args:
+            loads: List of applied loads in kN.
+            seasoned: True if seasoned timber is used and false otherwise.
+            moisture_content: precentage moisture content, given as whole numbers, e.g. for 15% set as 15.
+            latitude: True if located in coastal Queensland north of latitude 25 degrees south or 16 degrees south elsewhere, and False otherwise.
+            verbose: If True, print internal calculation details.
+
+        Returns:
+            A dictionary with bending capacities for different durations related to the factor k1
+        """
+        k4 = self._calc_k4(seasoned=seasoned,moisture_content=moisture_content,verbose=verbose)
+        k6 = self._calc_k6(latitude=latitude,verbose=verbose)
+        k12 = self._calc_k12_compression(pc=pc,g13=g13,La=La,slenderness=slenderness, out_of_plane=out_of_plane, verbose=verbose) 
+        Ac = self.breadth * self.depth
+        Ndc = self.φ_compression * k4 * k6 * k12 * fc * Ac
+        if verbose:
+            print(f"Ndc: {Ndc} KNm")
+        return Ndc
+
+    def _calc_k12_compression(self, pc:float|None = None, g13:float|None = None, La:float|None = None, slenderness:function|None = None, out_of_plane:bool|None = None, verbose:bool = True):
+        """Computes k12 using AS1720.1-2010 Cl 3.3.2.2"""
+        if  pc is None:
+            raise ValueError("pc not defined")
+        elif verbose:
+            print(f"pc: {pc}")
+        
+        if g13 is None:
+            raise ValueError("g13 not set. This should be selected from table 3.2. and relates to the end restraint of the column."\
+                             "Flat ends 0.7\n" \
+                            "Restrained at both ends in position and direction 0.7\n" \
+                            "Each end held by two bolts (substantially restrained) 0.75\n" \
+                            "One end fixed in position and direction, the other restrained in position only 0.85\n" \
+                            "Studs in light framing 0.9\n" \
+                            "Restrained at both ends in position only 1.0\n" \
+                            "Restrained at one end in position and direction and at the other end partially \n" \
+                            "restrained in direction but not in position 1.5\n" \
+                            "Restrained at one end in position and direction but not restrained in either \n" \
+                            "position or direction at other end 2.0\n")
+
+        S = slenderness(g13, La)
+        
+        if pc * S <= 10:
+            k12 = 1
+        elif pc * S <= 20:
+            k12 = 1.5 - 0.05 * pc * S
+        else:
+            k12 = 200/(pc*S)**2
+        if verbose:
+            print(f"k12: {k12}")
+        return k12
+
+    
+    
+    def _calc_S3(self,g13:float|None = None, Lax:float|None = None, verbose:bool = True):
+        if Lax is None:
+            raise ValueError("Lax not set. This is the distance between restraints...")
+        elif verbose:
+            print(f"Lax: {Lax} mm")
+        
+        S3 = min(Lax/self.depth, g13*self.length/self.depth)
+        if verbose:
+            print(f"S3: {S3}")
+        return S3
+        
+
+    def _calc_S4(self,g13:float|None = None, Lay:float|None = None, verbose:bool = True):
+        """ Computes Column slenderness for compression buckling using AS1720.1 Cl 3.3.2.2"""
+        if Lay is None:
+            raise ValueError("Lay not set. This is the distance between restraints...")
+        elif verbose:
+            print(f"Lay: {Lay} mm")
+        
+        S4 = min(Lay/self.breadth, g13*self.length/self.breadth)
+        if verbose:
+            print(f"S4: {S4}")
+        return S4
