e145573ccf13ff9f2a19c3249355145047d27294
Thomas Duffett <thomasduffett4@gmail.com>
2025-10-29 14:19:32 +1100

    moved _calc_fm to public masonry module

--- structures/Masonry/unreinforced_masonry.py
+++ structures/Masonry/unreinforced_masonry.py
@@ -5,6 +5,7 @@ AS3700:2018 for unreinforced masonry
 """
 
 import math
+from structures.Masonry import masonry
 from structures.util import round_half_up
 
 
@@ -138,7 +139,7 @@ class Clay:
         >>> from ..
         """
         km = self._calc_km(verbose=verbose)
-        self._calc_fm(km=km, verbose=verbose)
+        masonry.calc_fm(self=self, km=km, verbose=verbose)
 
         basic_comp_cap = round_half_up(
             self.phi_compression * self.fm,
@@ -1116,47 +1117,6 @@ class Clay:
         else:
             raise ValueError("Invalid mortar class provided")
         return km
-
-    def _calc_fm(
-        self,
-        km: float | None = None,
-        verbose: bool = True,
-    ):
-        """Computes fm in accordance with AS3700 Cl 3."""
-
-        if km is None:
-            raise ValueError("km not set.")
-        elif verbose:
-            print(f"km: {km}")
-        if self.hu is not None and self.tj is None:
-            raise ValueError(
-                "Masonry unit height provided but mortar thickness tj not provided"
-            )
-        elif self.hu is None and self.tj is not None:
-            raise ValueError(
-                "joint thickness tj provided but masonry unit height not provided"
-            )
-
-        kh = round_half_up(
-            min(
-                1.3 * (self.hu / (19 * self.tj)) ** 0.29,
-                1.3,
-            ),
-            self.epsilon,
-        )
-        if verbose:
-            print(
-                f"kh: {kh}, based on a masonry unit height of {self.hu} mm"
-                f" and a joint thickness of {self.tj} mm"
-            )
-
-        fmb = round_half_up(math.sqrt(self.fuc) * km, self.epsilon)
-        if verbose:
-            print(f"fmb: {fmb} MPa")
-
-        self.fm = round_half_up(kh * fmb, self.epsilon)
-        if verbose:
-            print(f"fm: {self.fm} MPa")
 
     def _calc_fms_horz(self, verbose: bool = True) -> float:
         fms_horizontal = min(0.15, max(1.25 * self.fm, 0.35))
